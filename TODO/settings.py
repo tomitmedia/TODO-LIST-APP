d79e71ad42ac511bbeb6db022cdc76a684726702
tomitmedia <tomitmedia1@gmail.com>
2021-06-17 00:55:04 +0100

    debug true

--- TODO/settings.py
+++ TODO/settings.py
@@ -26,8 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'ijbp17kld^$r_(15h1=qz1yri@ac=z3hw%gddocdil)u2i&66+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 
 #whitenoise_settings
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
