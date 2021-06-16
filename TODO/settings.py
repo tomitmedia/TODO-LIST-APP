9601e1e4141ebd9cc38123d68089ed60dfd1e464
tomitmedia <tomitmedia1@gmail.com>
2021-06-16 22:04:47 +0100

    started deployment

--- TODO/settings.py
+++ TODO/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    # 'django.db.models.BigAutoField',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+DEFAULT_AUTO_FIELD='django.db.models.AutoField'
